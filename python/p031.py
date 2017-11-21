2e03cbb3ac5f6ae4cdda0147829e1e2e754bb7bb
Luke Shays <lukeshays@google.com>
2017-11-21 14:14:57 -0500

    yeet

--- python/p031.py
+++ python/p031.py
@@ -1,11 +1,16 @@
-# How many different ways can £2 be made using any number of coins?
-# Values are 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
-
-coins = [1, 2, 5, 10, 20, 50, 100, 200]
-coinCounts = [200/c for c in coins]
-solutions = 0
+# How many different ways can 200p be made using any number of coins?
+# Values are 1p, 2p, 5p, 10p, 20p, 50p, 1 (100p) and 2 (200p).
 
 
-# Gives coin combos that add to goal using
-# at least one of max(coins)
+# Saw this algorithm on stackoverflow :(
 def coinCombos(coins, goal):
+    if goal == 0:
+        return 1
+    elif goal < 0 or not any(coins):
+        return 0
+    else:
+        return coinCombos(coins, goal-max(coins)) + coinCombos(coins[0:-1], goal)
+
+
+if __name__ == "__main__":
+    print coinCombos([1, 2, 5, 10, 20, 50, 100, 200], 200)
