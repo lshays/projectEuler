5f54b06de4df253783b3213084971e45a50e07e1
Luke Shays <lukeshays@gmail.com>
2017-11-25 14:19:20 -0500

    blah

---
+++ python/p037.py
@@ -0,0 +1,37 @@
+def primeSieve():
+    comps = {}
+    num = 2
+    while True:
+        if num not in comps:
+            comps[num**2] = [num]
+            yield num
+        else:
+            for factor in comps[num]:
+                comps.setdefault(factor + num, []).append(factor)
+            del comps[num]
+        num += 1
+
+
+def truncatable(n, primeList):
+    prime = str(n)
+    if len(prime) <= 1:
+        return False
+    for i in range(len(prime)):
+        if int(prime[i:]) not in primeList:
+            return False
+        if int(prime[:len(prime)-i]) not in primeList:
+            return False
+    return True
+        
+
+truncPrimes = []
+primes = []
+gen = primeSieve()
+while len(truncPrimes) < 11:
+    prime = next(gen)
+    primes.append(prime)
+    if truncatable(prime, primes):
+        truncPrimes.append(prime)
+
+print truncPrimes
+print sum(truncPrimes)
